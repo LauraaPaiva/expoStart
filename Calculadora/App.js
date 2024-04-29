e4d2995d7db2cf056d45a3f8a4398000958dbcbe
LauraaPaiva <lauraapaiva94@gmail.com>
2024-04-28 23:27:55 -0300

    Interface da calculadora

---
+++ Calculadora/App.js
@@ -0,0 +1,60 @@
+import { StatusBar } from "expo-status-bar";
+import { React, Component, Fragment } from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+import Display from "./components/Display";
+import Button from "./components/Button";
+
+export default class App extends Component {
+  state = {
+    displayValue: "0",
+  };
+
+  addDigit = (n) => {
+    this.setState({ displayValue: n });
+  };
+
+  clearMemory = () => {
+    this.setState({ displayValue: "0" });
+  };
+
+  setOperation = (o) => {};
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Display value={this.state.displayValue} />
+        <View style={styles.buttons}>
+          <Button label="AC" triple onClick={this.clearMemory} />
+          <Button label="/" operation onClick={() => this.setOperation("/")} />
+          <Button label="9" onClick={() => this.addDigit(9)} />
+          <Button label="8" onClick={() => this.addDigit(8)} />
+          <Button label="7" onClick={() => this.addDigit(7)} />
+          <Button label="*" operation onClick={() => this.setOperation("*")} />
+          <Button label="4" onClick={() => this.addDigit(4)} />
+          <Button label="5" onClick={() => this.addDigit(5)} />
+          <Button label="6" onClick={() => this.addDigit(6)} />
+          <Button label="-" operation onClick={() => this.setOperation("-")} />
+          <Button label="1" onClick={() => this.addDigit(1)} />
+          <Button label="2" onClick={() => this.addDigit(2)} />
+          <Button label="3" onClick={() => this.addDigit(3)} />
+          <Button label="+" operation onClick={() => this.setOperation("+")} />
+          <Button label="0" double onClick={() => this.addDigit(0)} />
+          <Button label="." onClick={() => this.addDigit(".")} />
+          <Button label="=" operation onClick={() => this.setOperation("=")} />
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#eee",
+  },
+  buttons: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+});
