d598303e20b50111b2fb11fe872f144fb0ef7fe4
LauraaPaiva <lauraapaiva94@gmail.com>
2024-04-29 08:06:37 -0300

    Implementação de calculos a calculadora

--- Calculadora/App.js
+++ Calculadora/App.js
@@ -5,20 +5,76 @@ import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import Display from "./components/Display";
 import Button from "./components/Button";
 
-export default class App extends Component {
-  state = {
-    displayValue: "0",
-  };
+initialState = {
+  displayValue: "0",
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+};
 
-  addDigit = (n) => {
-    this.setState({ displayValue: n });
+export default class App extends Component {
+  state = { ...initialState };
+
+  addDigit = (digit) => {
+    const clearDisplay =
+      this.state.displayValue === "0" || this.state.clearDisplay;
+
+    if (
+      digit === "." &&
+      !clearDisplay &&
+      this.state.displayValue.includes(".")
+    ) {
+      return;
+    }
+
+    const currentValue = clearDisplay ? "" : this.state.displayValue;
+
+    const displayValue = currentValue + digit; //texto concatenado
+
+    this.setState({
+      displayValue,
+      clearDisplay: false,
+    });
+
+    if (digit != ".") {
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[this.state.current] = newValue;
+      this.setState({ values });
+    }
   };
 
   clearMemory = () => {
-    this.setState({ displayValue: "0" });
+    this.setState({ ...initialState });
   };
 
-  setOperation = (o) => {};
+  setOperation = (operation) => {
+    if (this.state.current === 0) {
+      this.setState({
+        operation: operation,
+        current: 1,
+        clearDisplay: true,
+      });
+    } else {
+      const equals = operation === "=";
+      const values = [...this.state.values];
+      try {
+        values[0] = eval(values[0] + this.state.operation + values[1]);
+      } catch (e) {
+        values[0] = this.state.values[0];
+      }
+      values[1] = 0;
+
+      this.setState({
+        displayValue: values[0].toString(),
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: true,
+        values,
+      });
+    }
+  };
 
   render() {
     return (
@@ -27,9 +83,9 @@ export default class App extends Component {
         <View style={styles.buttons}>
           <Button label="AC" triple onClick={this.clearMemory} />
           <Button label="/" operation onClick={() => this.setOperation("/")} />
-          <Button label="9" onClick={() => this.addDigit(9)} />
-          <Button label="8" onClick={() => this.addDigit(8)} />
           <Button label="7" onClick={() => this.addDigit(7)} />
+          <Button label="8" onClick={() => this.addDigit(8)} />
+          <Button label="9" onClick={() => this.addDigit(9)} />
           <Button label="*" operation onClick={() => this.setOperation("*")} />
           <Button label="4" onClick={() => this.addDigit(4)} />
           <Button label="5" onClick={() => this.addDigit(5)} />
